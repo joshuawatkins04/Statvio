7f0a5355c1164841443613a06cf6c0367d2b47f8
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-12-23 23:42:12 +1100

    feat: add urlencoded feature and methods to deal with URL encoded payloads.

--- backend/src/middleware/middleware.js
+++ backend/src/middleware/middleware.js
@@ -19,9 +19,11 @@ const authenticateToken = (req, res, next) => {
 const configureMiddleware = (app) => {
   app.use(express.json());
   app.use(cookieParser());
+  app.use(express.urlencoded({ extended: true }));
   app.use(
     cors({
       origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
       credentials: true,
     })
   );

13b6b69ad3f73a817ceecdb1c20d75b96c388ee3
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-12-23 23:33:38 +1100

    feat(payment): add paypal routes for api integration.

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,20 +6,21 @@ const { configureMiddleware } = require("./middleware/middleware");
 const errorHandler = require("./middleware/errorHandler");
 const userRoutes = require("./routes/userRoutes");
 const spotifyRoutes = require("./routes/spotifyRoutes");
-const paymentRoutes = require("./routes/paymentRoutes");
+const paypalRoutes = require("./routes/paypalRoutes");
 
 const app = express();
 
 connectDatabase();
 configureMiddleware(app);
 
-app.use(session({ secret: "some-secret", resave: false, saveUninitialized: true }));
+app.use(
+  session({ secret: "some-secret", resave: false, saveUninitialized: true })
+);
 
 app.use("/api/auth", userRoutes);
 app.use("/music/spotify", spotifyRoutes);
-app.use("/paypal", paymentRoutes);
+app.use("/api/paypal", paypalRoutes);
 app.use(errorHandler);
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

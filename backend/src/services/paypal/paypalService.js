2ac0827854e8740512a01502ecfe2fe13ed71cae
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-12-24 15:43:21 +1100

    fix: incorrect brand_name now fixed.

--- backend/src/services/paypal/paypalService.js
+++ backend/src/services/paypal/paypalService.js
@@ -19,27 +19,20 @@ async function generateAccessToken() {
   }
 
   try {
-    const response = await paypalAxios.post(
-      "/v1/oauth2/token",
-      "grant_type=client_credentials",
-      {
-        auth: {
-          username: config.PAYPAL_CLIENT_ID,
-          password: config.PAYPAL_CLIENT_SECRET,
-        },
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
+    const response = await paypalAxios.post("/v1/oauth2/token", "grant_type=client_credentials", {
+      auth: {
+        username: config.PAYPAL_CLIENT_ID,
+        password: config.PAYPAL_CLIENT_SECRET,
+      },
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
     accessToken = response.data.access_token;
     accessTokenExpiry = currentTime + (response.data.expires_in - 60) * 1000;
     return accessToken;
   } catch (error) {
-    console.error(
-      "Failed to generate PayPal access token:",
-      error.response?.data || error.message
-    );
+    console.error("Failed to generate PayPal access token:", error.response?.data || error.message);
     throw new Error("Failed to generate PayPal access token");
   }
 }
@@ -58,10 +51,7 @@ async function checkOrderStatus(orderId) {
     console.log("Order Status:", response.data.status);
     return response.data.status;
   } catch (error) {
-    console.error(
-      "Failed to check order status:",
-      error.response?.data || error.message
-    );
+    console.error("Failed to check order status:", error.response?.data || error.message);
     throw new Error("Failed to check order status");
   }
 }
@@ -108,18 +98,13 @@ async function createOrder() {
     const response = await paypalAxios.post("/v2/checkout/orders", orderData, {
       headers,
     });
-    const approveLink = response.data.links.find(
-      (link) => link.rel === "approve"
-    );
+    const approveLink = response.data.links.find((link) => link.rel === "approve");
     if (!approveLink) {
       throw new Error("No approval link found in Paypal response.");
     }
     return approveLink.href;
   } catch (error) {
-    console.error(
-      "Failed to create PayPal order:",
-      error.response?.data || error.message
-    );
+    console.error("Failed to create PayPal order:", error.response?.data || error.message);
     throw new Error("Failed to create PayPal order");
   }
 }
@@ -133,19 +118,12 @@ async function capturePayment(orderId) {
     }
 
     const headers = await getAuthHeader();
-    const response = await paypalAxios.post(
-      `/v2/checkout/orders/${orderId}/capture`,
-      {},
-      { headers }
-    );
+    const response = await paypalAxios.post(`/v2/checkout/orders/${orderId}/capture`, {}, { headers });
 
     console.log("Payment capture response:", response.data);
     return response.data;
   } catch (error) {
-    console.error(
-      "Failed to capture payment:",
-      error.response?.data || error.message
-    );
+    console.error("Failed to capture payment:", error.response?.data || error.message);
     throw new Error("Failed to capture payment");
   }
 }

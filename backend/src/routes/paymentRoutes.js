82090015d37655e58d241374fa259e37f38a16a6
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-12-25 11:42:39 +1100

    delete: removed redundant file.

--- backend/src/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { captureOrder, createOrder } = require("../controllers/paymentController");
-
-const router = express.Router();
-
-router.post("/api/orders", createOrder(cart));
-router.post("/api/orders/:orderId/capture", captureOrder);
-
-module.exports = router;

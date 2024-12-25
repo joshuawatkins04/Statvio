8453a027cb319ecf36e9001e6588e7517a5e2296
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-12-25 11:24:38 +1100

    refactor: refactored error handling.

--- backend/src/controllers/paypalController.js
+++ backend/src/controllers/paypalController.js
@@ -1,23 +1,21 @@
 const paypalService = require("../services/paypal/paypalService");
 
-exports.createOrder = async (req, res) => {
+const createOrder = async (res, next) => {
   try {
     const url = await paypalService.createOrder();
     res.redirect(url);
   } catch (error) {
     console.error("Error creating order:", error);
-    res.status(500).json({ error: "Failed to create order" });
+    next(error);
   }
 };
 
-exports.capturePayment = async (req, res) => {
+const capturePayment = async (req, res, next) => {
   try {
     const token = req.query.token;
     if (!token) {
       console.error("Missing token in request query");
-      return res
-        .status(400)
-        .json({ error: "Missing token in query parameters" });
+      return res.status(400).json({ error: "Missing token in query parameters" });
     }
 
     console.log("Token received for payment capture:", token);
@@ -28,18 +26,17 @@ exports.capturePayment = async (req, res) => {
       data: captureResponse,
     });
   } catch (error) {
-    console.error("Error capturing payment:", error.message);
-
-    if (error.message.includes("Order already captured")) {
-      return res.status(200).json({ message: "Order already captured" });
-    }
-
-    res
-      .status(500)
-      .json({ error: "Failed to capture payment", details: error.message });
+    console.error("Error capturing order:", error);
+    next(error);
   }
 };
 
-exports.cancelOrder = (req, res) => {
+const cancelOrder = (res) => {
   res.redirect("/");
+};
+
+module.exports = {
+  createOrder,
+  capturePayment,
+  cancelOrder,
 };

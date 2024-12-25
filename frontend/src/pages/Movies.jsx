d0e4f162f433e481630c8ab46a535049a1ca6a45
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-12-26 08:21:41 +1100

    feat: new page for future addition of movie/tv show api's to the project.

---
+++ frontend/src/pages/Movies.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DefaultLayout from "../layouts/defaultLayout";
+
+const Movies = () => {
+  return (
+    <DefaultLayout>
+      <div>
+        MOVIES
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default Movies;

06cc5c5608b2e3fc1b0f7bd18319448b6a3acad6
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-12-27 15:48:49 +1100

    feat: apple music page draft.

---
+++ frontend/src/pages/AppleMusic.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DefaultLayout from "../layouts/DefaultLayout";
+
+const AppleMusic = () => {
+  return (
+    <DefaultLayout>
+      <div>Apple Music</div>
+    </DefaultLayout>
+  );
+};
+
+export default AppleMusic;

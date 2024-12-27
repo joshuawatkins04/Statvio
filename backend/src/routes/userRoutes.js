af51a3ee9b44de505b3451e8823cd2f958374921
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-12-27 15:43:23 +1100

    feat: /verify route on backend to do verification functionality.

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const userController = require("../controllers/userController");
-const authenticateToken = require("../middleware/middleware").authenticateToken;
+const { authenticateToken } = require("../middleware/authMiddleware");
+const { authLimiter, verifyLimiter } = require("../middleware/rateLimiter");
 
 const router = express.Router();
 
-router.post("/signup", userController.registerUser);
-router.post("/login", userController.loginUser);
+router.post("/signup", authLimiter, userController.registerUser);
+router.post("/login", authLimiter, userController.loginUser);
 
+router.get("/verify", authenticateToken, verifyLimiter, userController.verifyAuth);
 router.get("/dashboard", authenticateToken, userController.getUserDashboard);
 
 module.exports = router;

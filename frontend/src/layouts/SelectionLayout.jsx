e17c840b66494f4882d0935a4b84852af224b444
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-12-28 21:27:56 +1100

    fix: changed tailwindcss elements to respond to the light/dark mode theme changes.

--- frontend/src/layouts/SelectionLayout.jsx
+++ frontend/src/layouts/SelectionLayout.jsx
@@ -15,15 +15,15 @@ const SelectionLayout = ({ title, bodyText, connectToService, buttonText }) => {
   };
 
   return (
-    <div className="bg-surface text-onSurface shadow-lg rounded-xl p-6">
-      <h2 className="text-2xl font-semibold mb-4">{title}</h2>
-      <p className="mb-6">{bodyText}</p>
+    <div className="bg-surface shadow-lg rounded-xl p-6">
+      <h2 className="text-textPrimary text-2xl font-semibold mb-4">{title}</h2>
+      <p className="text-textSecondary mb-6">{bodyText}</p>
       <button
         onClick={handleClick}
         disabled={loading}
         className={`w-full flex items-center justify-center px-4 py-2 rounded-xl ${
-          loading ? "bg-gray-400 cursor-not-allowed" : "bg-blue-600 hover:bg-blue-700"
-        } text-white font-semibold transition duration-200`}
+          loading ? "bg-gray-400 cursor-not-allowed" : "bg-white hover:bg-gray-200"
+        } text-textSubtle font-semibold transition duration-200`}
       >
         {loading ? (
           <svg

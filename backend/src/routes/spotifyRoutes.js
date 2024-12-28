a992edf1bcd284318e0ed8b5f662272605549990
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-12-28 21:21:07 +1100

    feat: new routes for new backend routes for retrieving api data from spotify to provide the frontend with routes to retrieve this data.

--- backend/src/routes/spotifyRoutes.js
+++ backend/src/routes/spotifyRoutes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 router.get("/auth/spotify", musicController.redirectSpotifyAuth);
 router.get("/callback", musicController.spotifyCallback);
+router.get("/overview", musicController.getSpotifyOverview);
+router.get("/top-songs", musicController.getSpotifyTopSongs);
+router.get("/top-artists", musicController.getSpotifyTopArtists);
+router.get("/listening-history", musicController.getSpotifyListeningHistory);
 router.get("/playlists", musicController.getSpotifyPlaylists);
 router.get("/status", musicController.getSpotifyStatus);
 

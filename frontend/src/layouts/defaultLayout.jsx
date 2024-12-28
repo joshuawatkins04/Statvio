2d8eb3a4b32295b9c14c6e7fb259363e7f3ce93d
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-12-28 21:27:11 +1100

    fix: add margin-top to push down elements hidden under navbar component.

--- frontend/src/layouts/defaultLayout.jsx
+++ frontend/src/layouts/defaultLayout.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-const DefaultLayout = ({ children }) => {
+const DefaultLayout = ({ title, children }) => {
   return (
     <div className="min-h-screen p-6 bg-backdrop text-textPrimary transition-colors duration-300">
       <div className="w-full max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+        <header className="mt-24">
+        <h1 className="text-3xl font-bold text-center mb-6">{title}</h1>
+        </header>
         <main>{children}</main>
       </div>
     </div>

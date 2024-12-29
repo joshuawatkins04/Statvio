a6779418e5c6a2ca6a2508a5120581b56bb612bb
Joshua Watkins <joshua@watkinsfamily.com.au>
2024-12-29 23:44:05 +1100

    feat: add new routes for aws api routes for /upload and other future routes.

---
+++ backend/src/routes/awsRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const awsController = require("../controllers/awsController");
+const { upload } = require("../services/aws/aws");
+const { authenticateToken } = require("../middleware/authMiddleware");
+const router = express.Router();
+
+router.post("/upload", authenticateToken, upload.single("profileImage"), awsController.upload);
+
+module.exports = router;

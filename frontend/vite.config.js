a7edba0f4c59fa862de1b7c8787c40e480f4dc09
Joshua Watkins <joshua@watkinsfamily.com.au>
2025-01-03 21:47:33 +1100

    fix: fixed not changing to production properly.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE_");
-  const isProduction = mode === "production";
+
   return {
     plugins: [
       react({
         babel: {
-          plugins: isProduction ? [["transform-remove-console", { exclude: ["error", "warn"] }]] : [],
+          plugins: mode === "production" ? [["transform-remove-console", { exclude: ["error", "warn"] }]] : [],
         },
       }),
     ],

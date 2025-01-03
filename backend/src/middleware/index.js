7dca182735620a065fcd0853916e8b580a6783b2
Joshua Watkins <joshua@watkinsfamily.com.au>
2025-01-03 17:16:33 +1100

    More slight modifications.

--- backend/src/middleware/index.js
+++ backend/src/middleware/index.js
@@ -9,21 +9,26 @@ const configureMiddleware = (app) => {
 
   app.use(
     cors({
-      origin: ["http://localhost:5173", "http://3.107.192.136", "https://statvio.com"],
+      origin: ["http://localhost:5173", "http://3.107.192.136", "https://www.statvio.com", "https://statvio.com"],
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true,
     })
   );
 
+  app.options("*", (req, res) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.sendStatus(200);
+  });
+
   app.use(express.json());
   app.use(cookieParser());
   app.use(express.urlencoded({ extended: true }));
 
   app.use(globalLimiter);
-  app.options("*", (res) => {
-    res.sendStatus(200);
-  });
 };
 
 module.exports = { configureMiddleware };

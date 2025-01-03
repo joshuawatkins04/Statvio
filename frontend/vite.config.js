a065f045cf3f8391d9594c532859cb32cca6edb8
Joshua Watkins <joshua@watkinsfamily.com.au>
2025-01-03 18:45:42 +1100

    feat: auto remove debug command lines used in development for production.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: mode === "production" ? [["transform-remove-console", { exclude: ["error", "warn"] }]] : [],
+      },
+    }),
+  ],
   build: {
     outDir: "dist",
   },

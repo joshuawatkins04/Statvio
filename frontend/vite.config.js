ee9a52cdb469cf0810801b3007a600089d97bc9c
Joshua Watkins <joshua@watkinsfamily.com.au>
2025-01-03 18:58:55 +1100

    fix: bug with detecting mode of production.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: mode === "production" ? [["transform-remove-console", { exclude: ["error", "warn"] }]] : [],
-      },
-    }),
-  ],
-  build: {
-    outDir: "dist",
-  },
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: isProduction ? [["transform-remove-console", { exclude: ["error", "warn"] }]] : [],
+        },
+      }),
+    ],
+    build: {
+      outDir: "dist",
+    },
+  };
 });

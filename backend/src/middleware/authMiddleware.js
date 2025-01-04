0e8e7235172538db879792bac99f34a1f533c126
Joshua Watkins <joshua@watkinsfamily.com.au>
2025-01-04 14:55:58 +1100

    fix: changes to authenticating token logic to properly handle tokens.

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,13 +1,27 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
+  let token = null;
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  if (authHeader) {
+    token = authHeader.split(" ")[1];
+  }
+  if (!token && req.query.token) {
+    token = req.query.token;
+    console.warn("[Debug] Token fetched from query parameter.");
+  }
 
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
+  console.log("[Debug] Token Received:", token);
 
+  if (!token) {
+    console.warn("[Debug] No Token Found");
+    return res.status(401).json({ message: "Unauthorized" });
+  }
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: "Forbidden" });
+    if (err) {
+      console.error("[Debug] Token Verification Failed:", err.message);
+      return res.status(403).json({ message: "Forbidden" });
+    }
     req.user = user;
     next();
   });

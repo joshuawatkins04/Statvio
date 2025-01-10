30b2749feb2897f60c77148b5a14d7974ac4d49d
Joshua Watkins <joshua@watkinsfamily.com.au>
2025-01-11 08:15:46 +1100

    feat: draft implementation of soundcloud api integration.

---
+++ backend/src/controllers/soundcloud.js
@@ -0,0 +1,14 @@
+const { SoundcloudAuth, SoundcloudClient } = require ("../services/soundcloud");
+const User = require("../models/userModel");
+
+
+/* Controller functions */
+
+const scCallback = async (req, res) => {
+	
+}
+
+const getScPlaylists = handleScRequest(async (client) => {
+  const playlists = await client.getUserPlaylists();
+	return { playlists };
+});
---
+++ backend/src/services/soundcloud/index.js
@@ -0,0 +1,8 @@
+const SoundcloudAuth = require("./soundcloudAuth");
+const SoundcloudClient = require("./soundcloudClient");
+
+
+module.exports = { 
+    SoundcloudAuth,
+    SoundcloudClient,
+};

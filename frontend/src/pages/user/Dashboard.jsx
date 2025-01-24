feef63a02cc3d3052ccb8d756ca08a42ef4f8f54
Joshua Watkins <joshua@watkinsfamily.com.au>
2025-01-24 15:26:46 +1100

    feat/updates: /dashboard now properly implements tutorial feature for new users.

--- frontend/src/pages/user/Dashboard.jsx
+++ frontend/src/pages/user/Dashboard.jsx
@@ -4,9 +4,18 @@ import { AuthContext } from "../../contexts/AuthContext";
 import Subscribe from "./Subscribe";
 import DefaultLayout from "../../layouts/DefaultLayout";
 import ProfileImageUpload from "./ProfileImageUpload";
+import Tutorial from "./Tutorial";
 
 const Dashboard = () => {
   const { user, isAuthenticated, authLoading } = useContext(AuthContext);
+
+  const [tutorialComplete, setTutorialComplete] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      setTutorialComplete(user.tutorialComplete);
+    }
+  }, [user]);
 
   const [isUploadModalOpen, setIsUploadModalOpen] = useState(false);
   const [profileImage, setProfileImage] = useState(
@@ -42,130 +51,151 @@ const Dashboard = () => {
     );
 
   return (
-    <DefaultLayout title={"Your Dashboard"}>
-      {/* User Info Section */}
-      <div className="bg-surface text-onSurface rounded-xl shadow-card p-6 mb-6 shadow-lg hover:shadow-xl transition">
-        {/* Profile pic replacement */}
-        <div className="grid grid-cols-1 sm:grid-cols-2">
-          <div className="py-6 flex items-center justify-center space-x-8">
-            <img
-              src={profileImage}
-              className="w-36 h-36 object-cover rounded-full cursor-pointer"
-              alt="Profile"
-              onClick={handleProfileClick}
-            />
-          </div>
-          <div className="py-6">
-            {user ? (
-              <>
-                <div className="mb-4">
-                  <span className="text-textPrimary text-xl font-extrabold">
-                    Welcome <span className="text-primary">{user.username}</span>
-                  </span>
-                </div>
-                <div className="flex flex-col space-y-1">
-                <Link
-                    to="/settings?section=general"
-                    className="text-textSecondary font-semibold hover:underline flex items-center"
-                  >
-                    General Settings
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="ml-1 w-4 h-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
-                    </svg>
-                  </Link>
-                  <Link
-                    to="/settings?section=manage-profile"
-                    className="text-textSecondary font-semibold hover:underline flex items-center"
-                  >
-                    Manage Profile
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="ml-1 w-4 h-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
-                    </svg>
-                  </Link>
-                  <Link
-                    to="/settings?section=manage-api"
-                    className="text-textSecondary font-semibold hover:underline flex items-center"
-                  >
-                    Manage API's
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="ml-1 w-4 h-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
-                    </svg>
-                  </Link>
-                </div>
-              </>
-            ) : (
-              <p className="text-textSecondary">User not found</p>
-            )}
-          </div>
-        </div>
-      </div>
-
-      {/* Categories Section */}
-      <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-6 mb-6">
-        {[
-          {
-            name: "Music",
-            link: "/dashboard/music",
-          },
-          {
-            name: "Movies/TV",
-            link: "/dashboard/movies",
-          },
-          {
-            name: "Gaming",
-            link: "/dashboard/gaming",
-          },
-        ].map((category, index) => (
-          <Link to={category.link} key={index}>
-            <div className="bg-surface text-left p-6 shadow-lg rounded-xl hover:shadow-xl transition">
-              <h3 className="text-textPrimary text-lg font-semibold">{category.name}</h3>
-              <p className="text-textSecondary mt-2">Explore the latest trends and insights.</p>
-              <button className="border border-outline hover:border-surface text-textSecondary hover:text-white hover:bg-primary font-semibold mt-4 px-4 py-2 rounded-xl transition duration-200">
-                Explore {category.name}
-              </button>
+    <>
+      {!tutorialComplete ? (
+        <Tutorial />
+      ) : (
+        <DefaultLayout title={"Your Dashboard"}>
+          {/* User Info Section */}
+          <div className="bg-surface text-onSurface rounded-xl shadow-card p-6 mb-6 shadow-lg hover:shadow-xl transition">
+            {/* Profile pic replacement */}
+            <div className="grid grid-cols-1 sm:grid-cols-2">
+              <div className="py-6 flex items-center justify-center space-x-8">
+                <img
+                  src={profileImage}
+                  className="w-36 h-36 object-cover rounded-full cursor-pointer"
+                  alt="Profile"
+                  onClick={handleProfileClick}
+                />
+              </div>
+              <div className="py-6">
+                {user ? (
+                  <>
+                    <div className="mb-4">
+                      <span className="text-textPrimary text-xl font-extrabold">
+                        Welcome <span className="text-primary">{user.username}</span>
+                      </span>
+                    </div>
+                    <div className="flex flex-col space-y-1">
+                      <Link
+                        to="/settings?section=general"
+                        className="text-textSecondary font-semibold hover:underline flex items-center"
+                      >
+                        General Settings
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="ml-1 w-4 h-4"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M9 5l7 7-7 7"
+                          />
+                        </svg>
+                      </Link>
+                      <Link
+                        to="/settings?section=manage-profile"
+                        className="text-textSecondary font-semibold hover:underline flex items-center"
+                      >
+                        Manage Profile
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="ml-1 w-4 h-4"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M9 5l7 7-7 7"
+                          />
+                        </svg>
+                      </Link>
+                      <Link
+                        to="/settings?section=manage-api"
+                        className="text-textSecondary font-semibold hover:underline flex items-center"
+                      >
+                        Manage API's
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="ml-1 w-4 h-4"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M9 5l7 7-7 7"
+                          />
+                        </svg>
+                      </Link>
+                    </div>
+                  </>
+                ) : (
+                  <p className="text-textSecondary">User not found</p>
+                )}
+              </div>
             </div>
-          </Link>
-        ))}
-      </div>
-
-      {/* Payment Section */}
-      {/* <div className="bg-surface text-onSurface shadow-lg rounded-xl p-6">
-        <Subscribe />
-      </div> */}
-
-      {isUploadModalOpen && (
-        <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg">
-            <h2 className="text-xl font-semibold mb-4">Upload Profile Picture</h2>
-            <ProfileImageUpload onUploadSuccess={handleImageUpload} />
-            <button
-              onClick={() => setIsUploadModalOpen(false)}
-              className="mt-4 bg-red-500 text-white px-4 py-2 rounded"
-            >
-              Close
-            </button>
           </div>
-        </div>
+
+          {/* Categories Section */}
+          <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-6 mb-6">
+            {[
+              {
+                name: "Music",
+                link: "/dashboard/music",
+              },
+              {
+                name: "Movies/TV",
+                link: "/dashboard/movies",
+              },
+              {
+                name: "Gaming",
+                link: "/dashboard/gaming",
+              },
+            ].map((category, index) => (
+              <Link to={category.link} key={index}>
+                <div className="bg-surface text-left p-6 shadow-lg rounded-xl hover:shadow-xl transition">
+                  <h3 className="text-textPrimary text-lg font-semibold">{category.name}</h3>
+                  <p className="text-textSecondary mt-2">Explore the latest trends and insights.</p>
+                  <button className="border border-outline hover:border-surface text-textSecondary hover:text-white hover:bg-primary font-semibold mt-4 px-4 py-2 rounded-xl transition duration-200">
+                    Explore {category.name}
+                  </button>
+                </div>
+              </Link>
+            ))}
+          </div>
+
+          {/* Payment Section */}
+          {/* <div className="bg-surface text-onSurface shadow-lg rounded-xl p-6">
+        <Subscribe />
+        </div> */}
+
+          {isUploadModalOpen && (
+            <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-50">
+              <div className="bg-white p-6 rounded-lg shadow-lg">
+                <h2 className="text-xl font-semibold mb-4">Upload Profile Picture</h2>
+                <ProfileImageUpload onUploadSuccess={handleImageUpload} />
+                <button
+                  onClick={() => setIsUploadModalOpen(false)}
+                  className="mt-4 bg-red-500 text-white px-4 py-2 rounded"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          )}
+        </DefaultLayout>
       )}
-    </DefaultLayout>
+    </>
   );
 };
 
--- frontend/src/pages/user/Tutorial.jsx
+++ frontend/src/pages/user/Tutorial.jsx
@@ -1,12 +1,14 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useContext } from "react";
 import DefaultLayout from "../../layouts/DefaultLayout";
 import SectionGrid from "../../layouts/SectionGrid";
 import SectionList from "../../layouts/SectionList";
+import { updateTutorialStatus } from "../../hooks/user-management/userAuth";
+import { AuthContext } from "../../contexts/AuthContext";
 import { exampleTopSongs, exampleTopArtists, exampleListeningHistory } from "./TutorialContent";
 
 const Box = ({ title, body }) => {
   return (
-    <div className="animate-fadeIn select-none text-center bg-surface rounded-lg p-5 mb-10 shadow-md transition hover:scale-105 cursor-default">
+    <div className="animate-fadeIn select-none text-center bg-surface rounded-lg p-5 shadow-md transition hover:scale-105 cursor-default">
       <h1 className="text-textPrimary text-xl font-bold mb-4">{title}</h1>
       <p className="text-textSecondary">{body}</p>
     </div>
@@ -14,6 +16,7 @@ const Box = ({ title, body }) => {
 };
 
 const Tutorial = () => {
+  const { user } = useContext(AuthContext);
   const [click, setClick] = useState(false);
   const [render, setRender] = useState(false);
   const contentRef = useRef(null);
@@ -33,14 +36,25 @@ const Tutorial = () => {
     }, 0);
   };
 
+  const handleRedirect = async () => {
+    try {
+      const response = await updateTutorialStatus(true);
+      console.log(response);
+      window.location.reload();
+    } catch (error) {
+      console.error("Error redirecting:", error);
+    }
+  };
+
   return (
     <>
       <DefaultLayout title="How to use Statvio!" position="center">
-        {/* <div className="grid grid-rows-3 gap-2 md:grid-cols-3 md:gap-6"> */}
-        <div className="grid grid-cols-3 gap-6">
+        {/* <div className="grid grid-rows-3 gap-2 md:grid-cols-3 md:gap-6">  mt-32 md:mt-0*/}
+        <div className="grid grid-rows-3 md:grid-rows-none md:grid-cols-3 gap-6">
+          {/* <div className="grid grid-cols-3 gap-6"> */}
           <Box
-            title="1. First of all"
-            body="Welcome to Statvio! Your one-stop hub for connecting and managing all your favorite streaming services and apps in one place."
+            title="1. Welcome to Statvio!"
+            body="Your one-stop hub for connecting and managing all your favourite streaming services and apps in one place."
           />
           <Box
             title="2. How to use"
@@ -55,22 +69,29 @@ const Tutorial = () => {
         <div className="flex justify-center items-center animate-fadeIn">
           <button
             onClick={handleClick}
-            className="mt-10 hover:bg-primaryHover transition animate-bounce bg-primary rounded-md p-4 text-white font-semibold"
+            className="mt-10 hover:bg-primaryHover shadow-md hover:shadow-lg transition bg-primary rounded-md p-4 text-white font-semibold"
           >
             Connect to Statvio!
           </button>
         </div>
       </DefaultLayout>
-
-      <DefaultLayout title="Example Content">
-        {click && (
+      {click && (
+        <DefaultLayout title="Example Content">
           <div ref={contentRef} style={{ scrollMarginTop: "200px" }}>
+            <div className="flex justify-center items-center animate-fadeIn">
+              <button
+                onClick={handleRedirect}
+                className="hover:bg-primaryHover shadow-md hover:shadow-lg transition bg-primary rounded-md p-4 mb-5 text-white font-semibold"
+              >
+                Go to dashboard!
+              </button>
+            </div>
             <SectionGrid title="Top Songs" items={topSongs} loading={false} tutorial={true} />
             <SectionGrid title="Top Artists" items={topArtists} loading={false} tutorial={true} />
             <SectionList title="Listening History" items={listeningHistory} tutorial={true} />
           </div>
-        )}
-      </DefaultLayout>
+        </DefaultLayout>
+      )}
     </>
   );
 };
